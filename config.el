e14d5d99a77940f5fddeb0c5eb050c07e4e2353a
Gavin Hughes <gavhug@gmail.com>
2020-06-09 16:55:53 -0400

    Remove boilerplate. Add default fonts.

--- config.el
+++ config.el
@@ -2,28 +2,11 @@
 
 ;; Place your private configuration here. `doom sync` not required.
 
-(setq user-full-name "Gavin Hughes"
-     user-mail-address "gavhug@gmail.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "Iosevka" :size 16)
-;;       doom-variable-pitch-font (font-spec :family "Libre Baskerville")
-;;       doom-serif-font (font-spec :family "Libre Baskerville"))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'modus-operandi)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-;; (setq org-directory "~/Dropbox/OrgNotes/")
+(setq
+ user-full-name "Gavin Hughes"
+ user-mail-address "gavhug@gmail.com"
+ doom-font (font-spec :family "SF Mono" :size 20)
+ doom-big-font (font-spec :family "SF Mono" :size 36) ;; For presentations/streaming
+ doom-variable-pitch-font (font-spec :family "Avenir Next" :size 18)
+;; doom-serif-font (font-spec :family "Libre Baskerville")
+ org-directory "~/Dropbox/OrgNotes/")
