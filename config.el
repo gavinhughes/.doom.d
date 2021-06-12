2b62ddfd520de40e05509f50e6c74dbf3e30b4d4
Gavin Hughes <gavhug@gmail.com>
2021-06-12 12:46:55 +0800

    Standardize org insert headings. Cleanup.

--- config.el
+++ config.el
@@ -1,14 +1,17 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
 (setq
+  shell-file-name "/opt/homebrew/bin/fish"
+
   user-full-name "Gavin Hughes"
   user-mail-address "gavhug@gmail.com"
+
   doom-font (font-spec :family "DejaVu Sans Mono" :size 16)
   doom-big-font (font-spec :family "DejaVu Sans Mono" :size 30) ;; For presentations/streaming
   doom-variable-pitch-font (font-spec :family "DejaVu Sans Mono" :size 16)
   doom-serif-font (font-spec :family "DejaVu Serif")
-  doom-scratch-initial-major-mode 'org-mode
 
+  doom-scratch-initial-major-mode 'org-mode
   undo-limit 80000000                         ; Raise undo-limit to 80Mb
   evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
   auto-save-default t                         ; Nobody likes to loose work, I certainly don't
@@ -23,19 +26,15 @@
   esup-depth 0
   )
 
-(load! "my/functions")
+;; Maximize frame at startup.
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(setq org-roam-directory "~/Dropbox/OrgNotes/Roam")
-;; (require 'company-org-roam)
-    ;; (use-package company-org-roam
-    ;;   :when (featurep! :completion company)
-    ;;   :after org-roam
-    ;;   :config
-    ;;   (set-company-backend! 'org-mode '(company-org-roam company-yasnippet company-dabbrev)))
+(load! "my/functions")
 
 (after! org-roam
         :config
         (set-company-backend! 'org-mode '(company-org-roam company-yasnippet company-dabbrev)))
+(setq org-roam-directory "~/Dropbox/OrgNotes/Roam")
 
 ;; Not working yet
 ;; https://www.philnewton.net/blog/how-i-get-work-done-with-emacs/#fn.1
@@ -73,48 +72,32 @@
         "[X](D)"))
   org-todo-keyword-faces
       '(
-        ("TODO" :foreground "red" :weight bold)
+        ("TODO" :foreground "goldenrod4" :weight bold)
         ("WIP" :foreground "blue" :weight bold)
         ("DONE" :foreground "forest green" :weight bold)
         ("WAIT" :foreground "orange" :weight bold)
         ("CANCELLED" :foreground "forest green" :weight bold))
 )
 (after! org
-  (disable-flyspell)
+  ;;  (spell-fu-mode-disable)
   (vi-tilde-fringe-mode -1)
   (load! "my/checkboxes")
   (setq
         company-idle-delay nil ;; no autocompletion
-        org-hide-emphasis-markers t)
-  ;; These don't have the desired effect. They still do org-metaright,
-  ;; and demote/promote, just without including the subheadings.
-  ;; (define-key org-mode-map (kbd "S-M-<left>") nil)
-  ;; (define-key org-mode-map (kbd "S-M-<right>") nil)
-
-  ;; Org mode mappings
-  (map!
-   ;; These mappings are removed in favor of S-H and S-L.
-   ;; The original mappings available for use elsewhere.
-   ;; "S-<return>" (cmd! (org-insert-heading) (evil-append 1))
-   ;; "M-S-<return>" (cmd! (org-insert-subheading) (evil-append 1))
-   )
+        org-hide-emphasis-markers t
+        spell-fu-mode nil)
 )
 
 (map! :map org-mode-map
    "M-<left>" 'backward-word
    "M-<right>" 'forward-word
-   ;; "S-<return>" (cmd! (org-insert-heading) (evil-append 1))
-   ;; "M-S-<return>" (cmd! (org-insert-subheading) (evil-append 1))
-   ;; "M-S-<return>" (cmd! (org-insert-subheading) (evil-append 1))
-   ;; :n "S-<return>" 'org-insert-heading
-   ;; ;; :i "M-S-<return>" 'org-insert-subheading
-   ;; :i "S-<return>" 'org-insert-heading
-      )
+   "M-s-<return>" 'org-insert-subheading
+   "M-S-s-<return>" (cmd! (+org/insert-item-above 1) (org-cycle))
+)
+
 ;; Start new buffers in org-mode.
 ;;   Still get buffers in Fundamental mode using s-n.
 (map! :leader "b N" (cmd! (switch-to-buffer "*new*") (org-mode)))
-
-;;   (map! :map org-mode "M-S-s-<return>" #'org-insert-subheading-line-below)
 
 ; Don't spread text across the entire screen.
 (add-hook 'visual-line-mode-hook #'visual-fill-column-mode)
@@ -122,6 +105,7 @@
 (setq
   visual-fill-column-fringes-outside-margins nil
   visual-fill-column-center-text t
+  visual-fill-column-width 100
   )
 
 (after! inferior-ess-mode
@@ -140,40 +124,13 @@
         ;; "<s-mouse-1>" 'my-eww-open-in-new-window
   )
 
-;; Get this working to have windows in new frame
-(defun my-eww-open-in-new-window ()
-  (interactive)
-  (eww-open-in-new-buffer)
-  ;; (tear-off-window nil)
-  )
-
-
-;;;;; All this effort to get org-insert-subheading-line-below working.
-;; (map! :map org-mode
-;;        "M-S-s-return" "C-S-return escape 2*d p A tab") ;; org-insert-subheading-line-below
-;;
-;; Or as a function
-;; (defun org-insert-subheading-line-below ()
-;;   (interactive)
-;;   (+org/insert-item-above)
-;;   (evil-force-normal-state)
-;;   (evil-org-delete)
-;;   (evil-paste-after)
-;;   (evil-org-append-line)
-;;   (org-cycle)
-;;   (evil-org-append-line)
-;; )
-
-;; Maximize frame at startup.
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
 (map!
  "M-s-]"          'next-buffer
  "M-s-["          'previous-buffer
- "s-}"          'evil-window-next
- "s-{"          'evil-window-prev
- "C-c c"        'clone-indirect-buffer
- "s-\\"         'avy-goto-char
+ "s-}"            'evil-window-next
+ "s-{"            'evil-window-prev
+ "C-c c"          'clone-indirect-buffer
+ "s-\\"           'avy-goto-char
  "C-S-M-<return>" 'toggle-frame-fullscreen
  ;; This doesn't work in emacs-mac bc all frames are maximized.
  ;; "C-M-<return>" 'toggle-frame-maximized
@@ -191,23 +148,23 @@
 
  ;; ;; ROAM
  :leader "n SPC" 'org-roam-dailies-find-today
-
  )
 
 (map!
- ;; :n "b" 'avy-goto-char
+
+ ;; Movements
  :n "j" 'evil-next-visual-line
  :n "k" 'evil-previous-visual-line
 
- ;; Move cursor without going to normal mode.
- ;; Remaps goto-line
- :i "s-l" "<escape>la"
+ ;; Move without going to normal mode.
+ :i "s-l" "<escape>la"  ;; Remaps goto-line
  :i "s-h" "<escape>ha"
- :i "M-s-<return>" "<escape>ja"
+
+ ;; Character access
+ :i "M--" "–" ;; m-dash. Consistent with Mac.
 
  ;; These are for Haskell, Elixir. Scope them?
  ;; How to get the blank space after `->`?
- :i "M--" "–" ;; m-dash
  :i "M-s-;" (cmd! (insert "-> "))
  :i "M-s-:" (cmd! (insert "<- "))
  :i "M-s-." (cmd! (insert "|> "))
@@ -225,9 +182,6 @@
                 ("reg" "%(binary) -f %(ledger-file) reg")
                 ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
                 ("account" "%(binary) -f %(ledger-file) reg %(account)")))))
-
-(defun disable-flyspell () (flyspell-mode -1))
-
 
 ;; ;; (use-package org-download
 ;; ;;   :after org
