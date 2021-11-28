726b299ee917ab1d128cfececb18b4f08bed5812
Gavin Hughes <gavhug@gmail.com>
2021-11-28 10:51:20 -0500

    Add bibtex packages.

--- packages.el
+++ packages.el
@@ -1,34 +1,43 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; .doom.d/packages.el
 
-;; Disable unwanted Doom packages.
+;; Docs on configuration:
+;; https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#configuring-doom
+
 (disable-packages! evil-snipe)
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
 (package! openwith)
 (package! org-download)
+  ;; Dependencies: pngpaste
 (package! csv-mode)
-;; (package! octave)
 (package! osx-dictionary)
-(package! visual-fill-column)
-(package! dired+) ;; To reuse the same buffer when navigating the dir tree
+(package! dired+)
+  ;; To reuse the same buffer when navigating the dir tree
+  ;; TODO Not yet implemented
 (package! lorem-ipsum)
 (package! exec-path-from-shell)
 (package! org-anki)
 (package! reveal-in-osx-finder)
-
-;; http://pragmaticemacs.com/emacs/dynamically-filter-directory-listing-with-dired-narrow/
+(package! auto-compile)
 (package! dired-narrow)
-(package! company-org-roam
-   :recipe (:host github :repo "jethrokuan/company-org-roam"))
-(package! org-roam
-  :recipe (:host github :repo "org-roam/org-roam-v1"))
-  ;; :custom
-  ;;       (org-roam-directory "/Users/gavinhughes/Dropbox/OrgNotes/Roam")
-  ;; :bind (("C-c n l" . org-roam-buffer-toggle)
-  ;;        ("C-c n f" . org-roam-node-find))
-  ;; :config
-  ;;       (org-roam-setup))
+  ;; http://pragmaticemacs.com/emacs/dynamically-filter-directory-listing-with-dired-narrow/
+(unpin! org-roam)
+(package! org-roam-ui)
+(package! org-pandoc-import
+  ;; https://github.com/tecosaur/org-pandoc-import
+  :recipe (:host github
+           :repo "tecosaur/org-pandoc-import"
+           :files ("*.el" "filters" "preprocessors")))
+(package! org-appear)
+  ;; Auto-show markup symbols (=, *, etc) in org
+
+;; org-mac-link removed from org. applescript portion of file not working.
+;; Homepage: https://gitlab.com/aimebertrand/org-mac-link
+;; Try installing it again locally if it gets updated. [2021-11-26 Fri]
+;; ;; https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#usingloading-local-packages
+
+(package! org-roam-bibtex
+  :recipe (:host github :repo "org-roam/org-roam-bibtex"))
+
+;; When using bibtex-completion via the `biblio` module
+(unpin! bibtex-completion helm-bibtex ivy-bibtex)
